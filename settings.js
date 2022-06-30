b65dd8fcc22edbbc1aa2c801d7c5ff5d92745b54
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2022-06-30 13:02:27 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = true // auto read pesan / message
 //global.available = true //status online (online)
 
 // Other
-global.botname = "AZZBOT-MD" //namabot kalian
-global.ownername= "Yahya×፝֟͜×" //nama kalian
-global.myweb ="https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g" //bebas asal jan hapus
-global.github = "https://github.com/AzzBott679" //bebas
-global.email = "sdxshaders@gmaill.com" //bebas
+global.botname = "FIN-MD" //namabot kalian
+global.ownername= "፝֟FIN" //nama k"https://youtube.com/channel/Ualian
+global.myweb ="github.com/Raya2399" //bebas asal jan hapus
+global.youtube = "ALWANGANZZ OFFICIAL" //bebas asal jan hapus
+global.github = "https://github.com/Raya2399" //bebas
+global.email = "udrehan8@gmaill.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6281511480762" // nomor wa kalian
-global.ownernomerr = "+6281511480762" //nmr wa kalian
+global.ownernomer = "6282261065144" // nomor wa kalian
+global.ownernomerr = "+6282261065144" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://i.postimg.cc/L84bLpxL/menu-1.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6281511480762","6281511480762"] //ganti agar fitur owner bisa di gunakan
-global.packname = '©AzzBotོ' //sticker wm ubah
-global.author = 'YahyaGanzz' //sticker wm ganti nama kalian
+global.background_welcome="https://i.postimg.cc/B6jn1tkc/lol.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6282261065144","6282261065144"] //ganti agar fitur owner bisa di gunakan
+global.packname = '©FIN-md' //sticker wm ubah
+global.author = 'FIN' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -42,17 +42,17 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Trimakasih~',
+    sukses: '🤗Done, Oke desuu~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !!',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'COMMAND INI HANYA BISA DIGUNAKAN GEMPY',
+    owner: 'COMMAND INI HANYA BISA DIGUNAKAN FIN',
     group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
     private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
 	bot: 'Fitur Khusus Pengguna Nomor Bot',
     errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳Process...|| Join grup\nhttps://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2',
+    wait: '⏳Process...|| Join grup\nhttps://chat.whatsapp.com/JbUIV45JBqO199knmigsOP',
 	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'HALO @user SELAMAT DATANG DI @subject JANGAN LUPA BACA DESC\n\nJASA RUN BOT GEMPY\nHARGA MURMER\nCHAT AJA WA DIBAWAH\nwa.me/6287898307350\nBOT AKTIF 24JAM\TANPA INTERNET,BOT TETEP ON'
+	example1: 'HALO @user SELAMAT DATANG DI @subject JANGAN LUPA BACA DESC\nJANGAN LUPA JOIN GROUP OWNER\nlink gc1:\nhttps://chat.whatsapp.com/JbUIV45JBqO199knmigsOP\nlink gc2:\nhttps://chat.whatsapp.com/CJf6Hu8CK3m3YT1qI3qUVE\n\n\nSEMOGA BETAHH YA:>'
 }
 
 let file = require.resolve(__filename)
